fc2536120cac8f053a61304cc404faabc855624a
Ubuntu <ubuntu@ip-172-31-38-213.ap-south-1.compute.internal>
2026-05-27 07:19:50 +0000

    The monitoring script with enhancecd readability

--- monitor.sh
+++ monitor.sh
@@ -15,39 +15,50 @@
 #AWS IAM Users
 ###########################
 
+echo "Date and Time"
+echo
+date
+echo
+
 #List of S3 buckets
 
-#echo "List of S3 Buckets"
+echo "List of S3 Buckets"
 aws s3 ls
 
 #List of ec2 Instances
 
-#echo "List of ec2 InstanceId"
+echo "List of ec2 InstanceId"
 aws ec2 describe-instances --region ap-south-1  | jq .Reservations[].Instances[].InstanceId
 
-#echo "PublicIP"
+echo "PublicIP"
 aws ec2 describe-instances --region ap-south-1 | jq .Reservations[].Instances[].PublicIpAddress
 
-#echo "MAC Address"
+echo "MAC Address"
 aws ec2 describe-instances --region ap-south-1 | jq .Reservations[].Instances[].NetworkInterfaces[].MacAddress
 #List of Lambda functions
 
-#echo "List of Lambda functions"
+echo "List of Lambda functions"
 aws lambda list-functions --region ap-south-1
 
 #List of IAM Users
 
-#echo "List of IAM Users"
+echo "List of IAM Users"
 aws iam list-users
 
-#echo "CPU Usage"
+echo "###CPU Usage###"
+echo
 top -bn1 | grep "Cpu(s)";
+echo
 
-#echo "Memory Usage"
+echo "###Memory Usage###"
+echo
 free -h
+echo
 
-#echo "Disk Usage"
+echo "###Disk Usage###"
+echo
 df -h
+echo
 
 #END
 
